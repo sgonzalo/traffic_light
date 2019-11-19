992f4eb1cd4c7af75bb558e2cebaeeacacda710e
sgonzalo <santiago.gonzalo360@gmail.com>
2019-11-19 21:46:06 +0000

    first push

---
+++ src/js/component/TrafficLight.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+export default class TrafficLight extends React.Component {
+    constructor() {
+		super();
+		this.state = {
+			clickLight: null
+		}; 
+}
+
+render() {
+let redselect = ""; 
+		let yellowselect = "";
+		let greenselect = ""; 
+
+		switch (this.state.clickLight) {
+			case "red":
+				redselect = "selected";
+				break;
+			case "yellow":
+				yellowselect = "selected";
+				break;
+			case "green":
+				greenselect = "selected";
+				break;
+}
+
+return (
+<div>
+				<div id="TrafficTop" />
+				<div id="container">
+					<div
+						className={"red light " + redselect}
+						onClick={() => this.setState({ clickLight: "red" })}
+					/>
+					<div
+						className={"yellow light " + yellowselect}
+						onClick={() =>
+							this.setState({ clickLight: "yellow" })
+						}
+					/>
+					<div
+						className={"green light " + greenselect}
+						onClick={() => this.setState({ clickLight: "green" })}
+					/>
+				</div>
+			</div>
+		);
+	}
+}
+
+
+
+
+
--- src/js/index.js
+++ src/js/index.js
@@ -6,10 +6,12 @@ import ReactDOM from "react-dom";
 import "bootstrap";
 
 //include your index.scss file into the bundle
-import "../styles/index.scss";
+import "../styles/index.scss"; 
 
 //import your own components
-import { Home } from "./component/home.js";
+import { Home } from "./component/home.jsx";
+// import TrafficLight from "./component/TrafficLight.jsx";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
+ 
